a82a5d600e57c034474e9da04d8ad75f38682d44
Andrei Vlad Nicula <94231732+AndreyVLD@users.noreply.github.com>
2023-11-01 18:22:51 +0100

    Improved the CNN

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from sklearn.model_selection import train_test_split
-
 from mnist.SVM import SVMClassifier as SVM
 from mnist.CNN import CNNClassifier as CNN
 import numpy as np
 import pandas as pd
+from keras.datasets import mnist
 
 
 def file_printer(prediction, estimated_accuracy, filename):
@@ -21,33 +21,37 @@ mnist_28x28_uk = np.load('data/mnist_28x28_unknown.npy')
 def svm():
     svm_classifier = SVM(mnist_28x28, labels)
     svc_param_grid = {
-        'C': [2.745],
+        'C': [100],
         'kernel': ['rbf', ],
         'degree': [0, 1, 2],
-        'gamma': ['scale', 'auto'],
+        'gamma': [0.01],
         'random_state': [42]
     }
     print("\n-------------------------------------------------------------")
     print("SVC OUTPUT:")
     print("Best hyperparameters: " + svm_classifier.fine_tune_svm(param_grid=svc_param_grid).__str__())
     print("Accuracy: " + svm_classifier.cross_val_accuracy().__str__())
+    print("Accuracy: " + svm_classifier.overall_accuracy().__str__())
 
 
 # CNN
 def cnn():
     # Split data for CNN
-    mnist_28x28_train, mnist_28x28_test, labels_train, labels_test = train_test_split(mnist_28x28, labels,
-                                                                                      test_size=0.2, random_state=42)
+    # Our dataset:
+    x_train, x_test, y_train, y_test = train_test_split(mnist_28x28, labels,
+                                                        test_size=0.2, random_state=42)
+    # MNIST full dataset:
+    # (x_train, y_train), (x_test, y_test) = mnist.load_data()
     print("\n-------------------------------------------------------------")
     print("CNN OUTPUT:")
 
-    cnn_classifier = CNN(lr=0.01, batch_size=8)
-    cnn_classifier.train(mnist_28x28_train, labels_train, mnist_28x28_test, labels_test, epochs=30)
-    accuracy = cnn_classifier.evaluate(mnist_28x28_test, labels_test)
+    cnn_classifier = CNN(batch_size=8)  # batch_size=500 for the whole MNIST dataset
+    cnn_classifier.train(x_train, y_train, x_test, y_test, epochs=30)
+    accuracy = cnn_classifier.evaluate(x_test, y_test)
     cnn_classifier.plot_history()
 
     print("Accuracy: " + accuracy.__str__())
-    file_printer(cnn_classifier.predict(mnist_28x28_uk), accuracy, "GUD97.txt")
+    file_printer(cnn_classifier.predict(mnist_28x28_uk), accuracy, "group_69_classes.txt")
 
 
 choice = input("Enter 1 for SVM, 2 for CNN, 3 for both: ")
--- mnist/CNN.py
+++ mnist/CNN.py
@@ -1,58 +1,50 @@
 import numpy as np
-from keras.src.optimizers import Adam
-from keras.src.preprocessing.image import ImageDataGenerator
-from keras.src.utils import to_categorical
-from matplotlib import pyplot as plt
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.python.keras.callbacks import EarlyStopping, LearningRateScheduler
+import keras
+
+from keras.models import Model, Sequential
+from keras.layers import Dense
+from keras.layers import Conv2D, MaxPool2D, Dropout, Flatten
+from keras.callbacks import LearningRateScheduler
+import matplotlib.pyplot as plt
+from keras.utils import to_categorical
 
 
-def lr_schedule(x):
-    return 0.001 * 0.9 ** x
+def build_model():
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=5, activation='relu', input_shape=(28, 28, 1)))
+    model.add(MaxPool2D())
+    model.add(Conv2D(64, kernel_size=5, activation='relu'))
+    model.add(MaxPool2D())
+    model.add(Dropout(0.4))
+    model.add(Flatten())
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(10, activation='softmax'))
+    model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+    return model
 
 
 class CNNClassifier:
-    def __init__(self, lr=0.001, batch_size=32):
+    def __init__(self, batch_size=32):
         self.batch_size = batch_size
-        self.lr = lr
-        self.model = self.build_model()
+        self.model = build_model()
         self.history = None
 
-    def build_model(self):
-        model = keras.Sequential([
-            layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
-            layers.MaxPooling2D((2, 2)),
-            layers.Conv2D(64, (3, 3), activation='relu'),
-            layers.MaxPooling2D((2, 2)),
-            layers.Flatten(),
-            layers.Dense(128, activation='relu'),
-            layers.Dropout(0.5),
-            layers.Dense(10, activation='softmax')
-        ])
-        model.compile(optimizer=Adam(learning_rate=self.lr),
-                      loss='categorical_crossentropy',
-                      metrics=['accuracy'])
-        return model
-
     def train(self, X_train, y_train, X_test, y_test, epochs=5):
-        X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
-        X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
-        y_train = to_categorical(y_train, num_classes=10)
-        y_test = to_categorical(y_test, num_classes=10)
+        X_train = X_train / 255.0
+        X_test = X_test / 255.0
+        X_train = X_train.reshape(-1, 28, 28, 1)
+        X_test = X_test.reshape(-1, 28, 28, 1)
+        Y_train = to_categorical(y_train, num_classes=10)
+        Y_test = to_categorical(y_test, num_classes=10)
 
-        early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
-
-        datagen = ImageDataGenerator(rotation_range=10, width_shift_range=0.1, height_shift_range=0.1,
-                                     zoom_range=0.1)
-        self.history = self.model.fit(datagen.flow(X_train, y_train, batch_size=self.batch_size),
-                                      steps_per_epoch=len(X_train) / self.batch_size, epochs=epochs,
-                                      validation_data=(X_test, y_test),
-                                      callbacks=[LearningRateScheduler(lr_schedule), early_stopping])
+        annealer = LearningRateScheduler(lambda x: 1e-3 * 0.95 ** (x + epochs))
+        self.history = self.model.fit(X_train, Y_train, batch_size=self.batch_size, epochs=epochs, verbose=1,
+                                      validation_data=(X_test, Y_test), callbacks=[annealer])
 
     def evaluate(self, X_test, y_test):
-        X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
-        y_test = to_categorical(y_test, num_classes=10)
+        X_test = X_test.reshape(-1, 28, 28, 1)
+        y_test = keras.utils.to_categorical(y_test, num_classes=10)
         test_loss, test_accuracy = self.model.evaluate(X_test, y_test)
         return test_accuracy
 
